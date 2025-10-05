ffdcc7768fd67ba9f3ca1880ebc84f83d88749e5
Richard Chenyiwei Fu <chenyiweifu108@gmail.com>
2025-10-04 21:46:26 -0400

    clone

---
+++ data_cleaning/north/north_dataset/north.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import glob
+import os
+
+folder = "."
+csv_files = glob.glob(os.path.join(folder, "*.csv"))
+
+#join csvs on date column
+dfs = [pd.read_csv(f) for f in csv_files]
+df_merged = dfs[0]
+for i in range(1, len(dfs)):
+    df_merged = pd.merge(df_merged, dfs[i], on="time", how="outer")
+
+df_merged.to_csv("north_merged_all.csv", index=False)
+
+
+
---
+++ data_cleaning/south/south_dataset/south.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import glob
+import os
+
+folder = "."
+csv_files = glob.glob(os.path.join(folder, "*.csv"))
+#join csvs on date column
+
+dfs = [pd.read_csv(f) for f in csv_files]
+df_merged = dfs[0]
+for i in range(1, len(dfs)):
+    df_merged = pd.merge(df_merged, dfs[i], on="time", how="outer")
+
+df_merged.to_csv("south_merged_all.csv", index=False)
+print("✅ 所有 CSV 已按 Date 合并完成，保存为 merged_all.csv")
+
